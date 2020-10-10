e7d402a43bd514ab69edfa8c6cbe26155ed60bd0
Elizabeth Eidelson <fluturecode@gmail.com>
2020-10-09 23:03:07 -0400

    updated store, added login component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
-import Chat from './components/Chat';
+import Chat from "./components/Chat";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice.js";
 
 function App() {
+	const user = useSelector(selectUser);
 	return (
 		<div className='app'>
-			<Sidebar />
-			<Chat />
+			{user ? (
+				<>
+					<Sidebar />
+					<Chat />
+				</>
+			) : (
+				<h2>Please login</h2>
+			)}
 		</div>
 	);
 }
--- src/firebase.js
+++ src/firebase.js
@@ -14,3 +14,8 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
+
+// explicit imports
+export { auth, provider };
+// implicit imports
+export default db;
